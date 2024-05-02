3334bccea4d4f001da58b0b7825e2c80310df50e
Yasel Heredia Rojas <kkimerag@gmail.com>
2024-05-02 14:27:04 -0400

    enforcing HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if( ($this->app->environment('production')) || ($this->app->environment('test')) ) {
+            $this->app['request']->server->set('HTTPS','on');
+            URL::forceScheme('https');
+        }
     }
 }
